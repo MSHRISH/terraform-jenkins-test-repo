c8e4d6803192b2f2cad2b2ff60f4edc0cabc8dff
Shrish M <shrishm@presidio.com>
2024-12-10 15:44:39 +0530

    second commiot

--- main.tf
+++ main.tf
@@ -6,11 +6,7 @@ provider "aws" {
 
 # Create an S3 bucket
 resource "aws_s3_bucket" "bucket" {
-  bucket = var.bucket_name
-
-  tags = {
-    Name = var.bucket_name
-  }
+  bucket = "shrish-tf-bucket"
 }
 
 output "s3-bucket-id" {
